4220814f1fa14cc04cdc34717b24856404b6c569
Abdulfetah Yi. Abduljelil <abdulfetaheishak@gmail.com>
2024-02-23 15:10:50 +0300

    Add solution for Valid Triangle Number

---
+++ leetcode/valid-triangle-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        triangle_count, n = 0, len(nums)
+        for i in range(n - 2):
+            for j in range(i + 1, n - 1):
+                k = bisect_left(nums, nums[i] + nums[j], lo=j + 1) - 1
+                triangle_count += k - j
+        return triangle_count
